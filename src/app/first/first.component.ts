f2f554504acc0144485c7596c10e0f85c6a11a7d
AndreaPaciolla <andreapaciolla@gmail.com>
2021-11-03 11:33:02 +0100

    feat: first component

---
+++ src/app/first/first.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-first',
+  templateUrl: './first.component.html',
+  styleUrls: ['./first.component.css'],
+})
+export class FirstComponent implements OnInit {
+  @Input() company: string;
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  onButtonClick(evt: any) {
+    this.company = 'Click binding works';
+  }
+}
